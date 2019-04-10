883a2e394eb5132b00992da1692d6c8c1e0a7f1e
cm-fujii <fujii.genki@classmethod.jp>
2019-04-10 16:41:58 +0900

    Lambda1からLambda2を呼び出す

--- src/lambda1/app.py
+++ src/lambda1/app.py
@@ -1,6 +1,20 @@
+import os
 import json
+import boto3
 
 def lambda_handler(event, context):
+    client = boto3.client('lambda')
+
+    param = {
+        'hoge': 'fuga'
+    }
+
+    client.invoke(
+        FunctionName=os.getenv('TARGET_LAMBDA_FUNCTION'),
+        InvocationType='Event',
+        Payload=json.dumps(param)
+    )
+
     return {
         "statusCode": 200,
         "body": json.dumps({
